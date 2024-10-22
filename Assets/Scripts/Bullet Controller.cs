ea9b9d76c14958887ecae7b158002a8acd27950e
visheshvijayyy <w1911708@my.westminster.ac.uk>
2024-10-22 16:06:52 +0100

    Fixed `.gitignore` file also adding Assets

--- Assets/Scripts/Bullet Controller.cs
+++ Assets/Scripts/Bullet Controller.cs
@@ -4,10 +4,54 @@ using UnityEngine;
 
 public class BulletControlle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody rigidBody;
+
+    public float fireRate = 0.75f;
+
+    public GameObject bulletPrefab;
+    public Transform bulletPosition;
+    float nextFire;
+
+
+    public float bulletSpeed = 15f;
+
+    public void InitializeBullet(Vector3 originalDirection)
     {
-        
+        transform.forward = originalDirection;
+        rigidBody.velocity = transform.forward * bulletSpeed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+
+    void Fire()
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            GameObject bullet = Instantiate(bulletPrefab, bulletPosition.position, Quaternion.identity);
+
+            bullet.GetComponent<BulletControlle>()?.InitializeBullet(transform.rotation * Vector3.forward);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+        void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Move()
